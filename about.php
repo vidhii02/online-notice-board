8cf1115eea427fff1e026853b81d4fb20fe153df
vidhii02 <kodarlikarvidh02@gmail.com>
2023-01-30 00:25:43 +0530

    online notice board

---
+++ about.php
@@ -0,0 +1,3 @@
+<h2><b>ABOUT US</b></h2>
+<h3>This Notice board is made using php and mySql under the guidance of code clause and this was the project assigned to me in the duration of one month. This online notice board has admin login and users login. Admin can update password, Manage users and manage notifications i.e., add new notice or delete one.
+User can log in as well as sign up if he/she doesnt have an account. User can update password, update profile and Notifications i.e., if any notifications are available for the user or note</h3>
